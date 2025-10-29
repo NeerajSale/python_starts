dbe14e6dc34bc4e33d0df0d6300c829b93334330
Neeraj <neerajsale8@gmail.com>
2025-10-29 08:15:13 +0000

    home work2

---
+++ basics/variable1.py
@@ -0,0 +1,15 @@
+idno = 123456
+name = "neeraj"
+salary = 60000.00
+
+print(idno)
+print(name)
+print(salary)
+
+idno = 98765
+name ="charan"
+salary = 80000.00
+
+print(idno)
+print(name)
+print(salary)
---
+++ basics/variable2.py
@@ -0,0 +1,11 @@
+idno = 12345
+name = Neeraj 
+salary = 60000.00
+
+idno = 98765
+name = charan
+salary = 80000.00
+
+print(idno)
+print(name)
+print(salary)
