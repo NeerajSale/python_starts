46a8789569efe82a9bcc62c6d5f9c4a6c09e5ea4
Neeraj <neerajsale8@gmail.com>
2025-11-04 23:55:17 +0000

    page23_24homework

---
+++ basics/page23_24.py
@@ -0,0 +1,94 @@
+a = 5
+b = 3
+b = a
+print(a,b)
+
+a = 3
+b = 2.5
+print(a,b)
+
+a = 2.5
+b = 5 
+c = a+b
+print(c)
+
+a = "sathya"
+b = "tech"
+c = (a+b)
+print(c)
+
+a = 2
+b = 5
+c = a,b
+print(c)
+
+a = 2.3
+b = 9 
+b = a
+print(b)
+
+a = 5
+b = 3
+c = a+b
+print(c)
+
+x = 'a'
+y = 5 
+y = x
+print(x,y)
+
+x = 2.3
+y = 3 
+z = x+y
+print(z)
+
+a = 'x'
+b = 'y'
+c = (a,b)
+print(c)
+
+x = 5
+y = 2 
+z = x//y
+print(z)
+
+x = 5
+y = x*x
+print(y)
+
+x = 3
+y = 3*3*3 # x ** 3
+print(x,y)
+
+x = 3
+s = 3*3 # x ** 2
+c = 3*3*3 # x ** 3
+print (s,c)
+
+x = 2
+s = x ** 2
+c = x ** 3
+sum = (s+c)
+print(sum)
+
+x = 5
+y = 3 
+z = x*y
+p = x+y+z
+print(p)
+
+sub1 = 60
+sub2 = 70 
+sub3 = 80
+total_marks = sub1+sub2+sub3
+percentage = total_marks/300*100
+print(total_marks)
+print(percentage)
+
+product_cost = 1000
+gst = 1000/100*10
+net_price = product_cost + gst
+print(net_price)
+
+
+
